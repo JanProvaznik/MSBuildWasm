e3ac9381a5b4f6e9d072da436817363535d1bf6e
Jan Provaznik <t-jprovaznk@microsoft.com>
2024-06-26 16:55:15 +0200

    add test project

---
+++ test/WasmTasksTests/WasmTask_Tests.cs
@@ -0,0 +1,17 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.Build.Framework;
+using WasmWasiTasks;
+
+namespace WasmTasksTests
+{
+    public class WasmTask_Tests
+    {
+        [Fact]
+        public void Execute_WithDefaultSettings_ShouldSucceed()
+        {
+        }
+
+    }
+}
