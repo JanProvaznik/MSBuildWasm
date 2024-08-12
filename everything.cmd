a1b0d4e00de92faa970eea2d9b9d93fa61a32f59
Jan Provaznik <t-jprovaznk@microsoft.com>
2024-08-12 10:18:07 +0200

    remove accidentally added files

--- everything.cmd
+++
@@ -1,6 +0,0 @@
-rm *.nupkg
-dotnet nuget locals global-packages --clear
-dotnet build .\src\ -c Release
-dotnet pack .\src\ -o .
-dotnet add .\examples\ExampleProject\ExampleProject.csproj package MSBuildWasm -s .\ --prerelease
-dotnet build .\examples\
